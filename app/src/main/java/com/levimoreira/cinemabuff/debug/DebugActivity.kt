fca5a92356aa1e9538ebf047da6807e65cd9d0ee
Levi Albuquerque <Levi.X.Albuquerque.-ND@disney.com>
2020-02-16 16:34:50 -0300

    Add debug menu

---
+++ app/src/main/java/com/levimoreira/cinemabuff/debug/DebugActivity.kt
@@ -0,0 +1,13 @@
+package com.levimoreira.cinemabuff.debug
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.levimoreira.cinemabuff.R
+
+class DebugActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_debug)
+    }
+}
--- app/src/main/java/com/levimoreira/cinemabuff/navigation/NavigationActivity.kt
+++ app/src/main/java/com/levimoreira/cinemabuff/navigation/NavigationActivity.kt
@@ -1,10 +1,16 @@
 package com.levimoreira.cinemabuff.navigation
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.levimoreira.cinemabuff.DebugActivity
 import com.levimoreira.cinemabuff.R
 
 class NavigationActivity : AppCompatActivity() {
@@ -15,5 +21,26 @@ class NavigationActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.menu_about -> {
+                Toast.makeText(this, getString(R.string.about), Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.menu_debug -> {
+                startActivity(Intent(this, DebugActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
